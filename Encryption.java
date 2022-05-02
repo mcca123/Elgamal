71288e9473ee9b446c511900037e25927f5b8158
Flying <b.noppawat@gmail.com>
2022-05-02 20:09:48 +0700

    little fixed

--- Encryption.java
+++ Encryption.java
@@ -6,21 +6,28 @@ public class Encryption {
         // System.out.println("ran: " + generatedLong);
         // long r = Math.abs(generatedLong);
         // System.out.println("ran po: " + r);
-        long x = 563;
-        long[] cipher = encryption(563, 128, 389, x);
+        long x = 47857;
+        long p = 2189725403L;// 11
+        long g = 619226901;// 2
+        long y = 1017512232;// 2
+        long u = 1027894598;// 2
+
+        System.out.println("Plain text:" + x);
+        System.out.println("==Encryption==");
+        long[] cipher = encryption(p, g, y, x);
         System.out.println("a :" + cipher[0]);
         System.out.println("b :" + cipher[1]);
-
-        long plaintext = decryption(563, 128, 65, cipher[0], cipher[1]);
-        System.out.println("plain: " + plaintext);
+        System.out.println("==Decryption==");
+        long plaintext = decryption(p, g, u, cipher[0], cipher[1]);
+        System.out.println("plain text: " + plaintext);
 
     }
 
     public static long[] encryption(long p, long g, long y, long x) {
-        // long k = 1 + (long) (Math.random() * (rightLimit - 1));
         long k = 0;
         while (FindPrime.gcd(k, p - 1) != 1) {
-            k = Math.abs(new Random().nextLong());
+            k = 1 + (long) (Math.random() * (p));
+            System.out.println("k: " + k);
         }
 
         long a = FindPrime.fastExponent(g, k, p);
@@ -33,8 +40,9 @@ public class Encryption {
     public static long decryption(long p, long g, long u, long a, long b) {
         System.out.println("a:" + a);
         System.out.println("b:" + b);
-        // long k = 1 + (long) (Math.random() * (rightLimit - 1));
-        long plaintext = (b * FindPrime.fastExponent(a, (p - 1 - u), p))%p;
+
+        long plaintext = (b * FindPrime.fastExponent(a, (p - 1 - u), p)) % p;
+        // long plaintext = (b / FindPrime.fastExponent(a, u, p))%p;
         return plaintext;
 
     }
