67833d3755e9f799e0d8a6967afda1955c81d98a
Flying <b.noppawat@gmail.com>
2022-05-30 19:16:46 +0700

    Create CreateFile.java

---
+++ CreateFile.java
@@ -0,0 +1,20 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args) {
+        String filename = "a.txt";
+        String text = "mcca\ndasda\ndas\n*-*";
+        stringToFile(filename, text);
+    }
+    public static void stringToFile(String filename,String text){
+        try {
+            FileWriter myWriter = new FileWriter(filename);
+            myWriter.write(text);
+            myWriter.close();
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+    }
+}
